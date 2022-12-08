5b824ad63823a220406e02de63273e067069bdb2
Dannel Albert <cuebix@gmail.com>
2022-12-07 23:53:49 -0500

    feat: solve day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -42,7 +42,21 @@ pub mod part1 {
     }
 }
 
-#[derive(Debug)]
+pub mod part2 {
+    use super::*;
+    use std::{env::current_dir, fs::read_to_string};
+
+    pub fn solution() {
+        let filename = current_dir().unwrap().join("src/data/day2.txt");
+        let input = read_to_string(filename).unwrap();
+
+        let total_score = calculated_rock_paper_scissors_tournament_score_with_elf_strategy(&input);
+
+        println!("Total Score = {}", total_score);
+    }
+}
+
+#[derive(Debug, Clone)]
 enum Shape {
     Rock = 1,
     Paper = 2,
@@ -50,10 +64,32 @@ enum Shape {
 }
 
 #[derive(Debug)]
+enum Goal {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl TryFrom<&str> for Goal {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "X" => Ok(Self::Lose),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => Err("Invalid Goal Token"),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Round {
-    _my_shape: Shape,
+    _my_shape_my_strategy: Shape,
+    _my_shape_elf_strategy: Shape,
     _their_shape: Shape,
-    my_score: u32,
+    my_score_my_strategy: u32,
+    my_score_elf_strategy: u32,
 }
 
 impl TryFrom<&str> for Round {
@@ -65,15 +101,23 @@ impl TryFrom<&str> for Round {
         let my_token = parts.pop().unwrap();
         let their_token = parts.pop().unwrap();
 
-        let my_shape = Shape::try_from(my_token)?;
+        let my_shape_my_strategy = Shape::try_from(my_token)?;
         let their_shape = Shape::try_from(their_token)?;
 
-        let my_score = my_shape.point_value() + my_shape.score_against(&their_shape);
+        let my_goal = Goal::try_from(my_token)?;
+        let my_shape_elf_strategy = Shape::meets_goal(&my_goal, &their_shape);
+
+        let my_score_my_strategy =
+            my_shape_my_strategy.point_value() + my_shape_my_strategy.score_against(&their_shape);
+        let my_score_elf_strategy =
+            my_shape_elf_strategy.point_value() + my_shape_elf_strategy.score_against(&their_shape);
 
         Ok(Round {
-            _my_shape: my_shape,
+            _my_shape_my_strategy: my_shape_my_strategy,
+            _my_shape_elf_strategy: my_shape_elf_strategy,
             _their_shape: their_shape,
-            my_score,
+            my_score_my_strategy,
+            my_score_elf_strategy,
         })
     }
 }
@@ -120,6 +164,23 @@ impl Shape {
         }
     }
 
+    fn meets_goal(goal: &Goal, opponent: &Self) -> Self {
+        match goal {
+            Goal::Lose => match opponent {
+                Shape::Rock => Shape::Scissors,
+                Shape::Paper => Shape::Rock,
+                Shape::Scissors => Shape::Paper,
+            },
+            Goal::Draw => opponent.clone(),
+            Goal::Win => match opponent {
+                Shape::Rock => Shape::Paper,
+                Shape::Paper => Shape::Scissors,
+                Shape::Scissors => Shape::Rock,
+            },
+        }
+    }
+}
+
 pub fn calculated_rock_paper_scissors_tournament_score_with_my_strategy(input: &str) -> u32 {
     let mut total_score = 0;
 
@@ -135,7 +196,7 @@ pub fn calculated_rock_paper_scissors_tournament_score_with_my_strategy(input: &
     total_score
 }
 
-pub fn calculated_rock_paper_scissors_tournament_score(input: &str) -> u32 {
+pub fn calculated_rock_paper_scissors_tournament_score_with_elf_strategy(input: &str) -> u32 {
     let mut total_score = 0;
 
     for line in input.lines() {
@@ -144,7 +205,7 @@ pub fn calculated_rock_paper_scissors_tournament_score(input: &str) -> u32 {
         }
 
         let round = Round::try_from(line).unwrap();
-        total_score += round.my_score;
+        total_score += round.my_score_elf_strategy;
     }
 
     total_score
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use advent_of_code_2022::day2;
 
 fn main() {
-    day2::part1::solution();
+    day2::part2::solution();
 }
