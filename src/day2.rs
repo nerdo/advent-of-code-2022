25b1285ea53175599bdb75976b66e242883feea4
Dannel Albert <cuebix@gmail.com>
2022-12-07 23:11:40 -0500

    test: add empty lines in input

--- src/day2.rs
+++ src/day2.rs
@@ -6,7 +6,9 @@ mod tests {
     fn calculated_rock_paper_scissors_tournament_score_returns_the_correct_score() {
         let input = "A Y
 B X
-C Z";
+C Z
+
+";
         let total_score = calculated_rock_paper_scissors_tournament_score(&input);
 
         assert_eq!(total_score, 15);
