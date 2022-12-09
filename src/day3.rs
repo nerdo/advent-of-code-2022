e6baeee33f3b71d8df3dd5b4e4bf27455303125e
Dannel Albert <cuebix@gmail.com>
2022-12-08 22:10:37 -0500

    test: add test case, data for day 3

---
+++ src/day3.rs
@@ -0,0 +1,60 @@
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_misplaced_item_priority_sum_returns_the_correct_value() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+            ";
+
+        let answer = get_misplaced_item_priority_sum(input);
+
+        assert_eq!(answer, 157);
+    }
+}
+
+pub fn get_misplaced_item_priority_sum(input: &str) -> u32 {
+    let mut misplaced_item_priority_sum: u32 = 0;
+
+    for line in input.lines() {
+        let line = line.trim();
+
+        if line.is_empty() {
+            continue;
+        }
+
+        let compartment_size = line.len() / 2;
+
+        let first_compartment = &line[..compartment_size];
+        let second_compartment = &line[compartment_size..];
+
+        let matching_item_type =
+            find_matching_item_type(first_compartment, second_compartment).unwrap();
+
+        let misplaced_item_priority = match matching_item_type as u8 {
+            b'a'..=b'z' => (matching_item_type as u8) - b'a' + 1,
+            b'A'..=b'Z' => (matching_item_type as u8) - b'A' + 27,
+            _ => panic!("Item is not a letter!"),
+        };
+
+        misplaced_item_priority_sum += misplaced_item_priority as u32;
+    }
+
+    misplaced_item_priority_sum
+}
+
+fn find_matching_item_type<'a>(a: &'a str, b: &'a str) -> Result<char, &'static str> {
+    for item_type in a.chars() {
+        if b.contains(item_type) {
+            return Ok(item_type);
+        }
+    }
+
+    Err("No Matching Item Type Found")
+}
