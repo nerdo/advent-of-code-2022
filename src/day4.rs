22401bb50ae183d0fe733eca6c70c0fcd85028f1
Dannel Albert <cuebix@gmail.com>
2022-12-10 15:21:42 -0500

    refactor: implement get_num_assignments_fully_contains_other_in_pair

--- src/day4.rs
+++ src/day4.rs
@@ -9,7 +9,9 @@ mod tests {
 5-7,7-9
 2-8,3-7
 6-6,4-6
-2-6,4-8";
+2-6,4-8
+    
+";
 
         let answer = get_num_assignments_fully_contains_other_in_pair(input);
 
@@ -18,5 +20,40 @@ mod tests {
 }
 
 pub fn get_num_assignments_fully_contains_other_in_pair(assignment_list: &str) -> u32 {
-    2
+    let mut num_assignments_fully_contains_other = 0;
+
+    let range_contains = |a: (u32, u32), b: (u32, u32)| a.0 <= b.0 && a.1 >= b.1;
+    let parse_range = |input: &str| {
+        let parts: Vec<&str> = input.split('-').collect();
+        if parts.len() != 2 {
+            return Err("Invalid Range");
+        }
+
+        let start = parts.first().unwrap().parse::<u32>().unwrap();
+        let end = parts.get(1).unwrap().parse::<u32>().unwrap();
+        Ok((start, end))
+    };
+
+    for assignment_line in assignment_list.lines() {
+        let (range_a, range_b) = {
+            let parts: Vec<&str> = assignment_line.split(',').collect();
+            if parts.len() != 2 {
+                continue;
+            }
+            let Ok(a) = parse_range(parts.first().unwrap()) else {
+                continue;
+            };
+            let Ok(b) = parse_range(parts.get(1).unwrap()) else {
+                continue;
+
+            };
+            (a, b)
+        };
+
+        if range_contains(range_a, range_b) || range_contains(range_b, range_a) {
+            num_assignments_fully_contains_other += 1;
+        }
+    }
+
+    num_assignments_fully_contains_other
 }
