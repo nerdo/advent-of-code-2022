343f1c6af74c7f7584893a0f6d94906b95f15d69
Dannel Albert <cuebix@gmail.com>
2022-12-11 12:01:08 -0500

    test: re-design with enums
    this feels like the right direction

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,6 @@
+#![warn(missing_docs)]
+//! Advent of Code 2022 Day 7 Solution
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,35 +32,68 @@ $ ls
 5626152 d.ext
 7214296 k
             ";
-        let file_system = Directory::from_terminal_replay(terminal_replay);
+        let file_system = FileSystemEntry::from_terminal_replay(terminal_replay);
 
         let answer = file_system.get_total_size(Criteria {
-            file_size_range: (0, 100000),
+            size_range: (0, 100000),
         });
 
         assert_eq!(answer, 95437);
     }
 }
 
+/// Part 1.
 pub mod part1 {
+    /// The solution for Part 1.
     pub fn solution() {}
 }
 
+/// Part 2
 pub mod part2 {
+    /// The solution for Part 2.
     pub fn solution() {}
 }
 
-pub struct Directory {}
+/// Represents a file system entry.
+pub enum FileSystemEntry {
+    /// Represents a directory in a file system.
+    Directory {
+        /// The name of the directory.
+        name: String,
+        /// The size of the directory and its contents.
+        size: usize,
+        /// The list of file system entries contained within the directory. This can be a mix of
+        /// both files and directories.
+        entries: Vec<FileSystemEntry>,
+    },
 
-pub struct Criteria {
-    file_size_range: (u64, u64),
+    /// Represents a file in a file system.
+    File {
+        /// The name of the file.
+        name: String,
+        /// The size of the file.
+        size: usize,
+    },
 }
 
-impl Directory {
+/// Criteria for [`get_total_size`]: #method.get_total_size
+pub struct Criteria {
+    /// The size range to constrain [`get_total_size`]: #method.get_total_size to.
+    size_range: (u64, u64),
+}
+
+impl FileSystemEntry {
+    /// Parses terminal replay output into a `FileSystem::Directory` variant containing the file
+    /// system structure.
     pub fn from_terminal_replay(terminal_replay: &str) -> Self {
-        Self {}
+        Self::Directory {
+            name: "/".to_string(),
+            size: 0,
+            entries: vec![],
+        }
     }
 
+    /// Gets total size of file system entry
     pub fn get_total_size(&self, criteria: Criteria) -> u64 {
         95437
     }
