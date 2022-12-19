4c92245b49ee992a6ff554936d01d56a16c264d9
Dannel Albert <cuebix@gmail.com>
2022-12-18 22:58:50 -0500

    test: implement parse with rope length

--- src/day9.rs
+++ src/day9.rs
@@ -6,7 +6,6 @@ use std::{
     collections::HashSet,
     fmt::Display,
     ops::{Add, Sub},
-    str::FromStr,
 };
 
 use anyhow::{anyhow, bail, Error};
@@ -30,11 +29,59 @@ R 2
 
             ";
 
-        let rope = Rope::from_str(input)?;
+        let rope = Rope::parse(input, 2)?;
 
         let answer = rope.get_number_of_positions_rope_tail_visits_at_least_once();
 
         assert_eq!(answer, 13);
+
+        Ok(())
+    }
+
+    #[test]
+    fn get_number_of_positions_rope_tail_visits_at_least_once_with_ten_knots_sanity_check(
+    ) -> Result<(), Error> {
+        let input = "
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+
+            ";
+
+        let rope = Rope::parse(input, 10)?;
+
+        let answer = rope.get_number_of_positions_rope_tail_visits_at_least_once();
+
+        assert_eq!(answer, 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn get_number_of_positions_rope_tail_visits_at_least_once_with_ten_knots_complex(
+    ) -> Result<(), Error> {
+        let input = "
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+
+            ";
+
+        let rope = Rope::parse(input, 10)?;
+
+        let answer = rope.get_number_of_positions_rope_tail_visits_at_least_once();
+
+        assert_eq!(answer, 36);
 
         Ok(())
     }
@@ -51,7 +98,7 @@ pub mod part1 {
         let filename = current_dir()?.join("src/data/day9.txt");
         let input = read_to_string(filename)?;
 
-        let rope = Rope::from_str(&input)?;
+        let rope = Rope::parse(&input, 2)?;
 
         let answer = rope.get_number_of_positions_rope_tail_visits_at_least_once();
 
@@ -108,13 +155,110 @@ pub struct Rope {
     tail_positions_visited: Vec<Position>,
 }
 
-impl FromStr for Rope {
-    type Err = Error;
+impl Rope {
+    /// Processes one movement of the rope and returns where all the trailing knot should be.
+    fn get_updated_positions(direction: &str, knots: &[Position]) -> Result<Vec<Position>, Error> {
+        let mut updated_knots = Vec::with_capacity(knots.len());
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut head_position = Position { x: 0, y: 0 };
-        let mut tail_position = Position { x: 0, y: 0 };
+        let Some(&start_position) = knots.first() else {
+            bail!("No head knot!");
+        };
+
+        let mut head_position = start_position;
+
+        // Move the head.
+        match direction {
+            "R" => {
+                head_position = Position {
+                    x: head_position.x + 1,
+                    y: head_position.y,
+                };
+            }
+            "L" => {
+                head_position = Position {
+                    x: head_position.x - 1,
+                    y: head_position.y,
+                };
+            }
+            "U" => {
+                head_position = Position {
+                    x: head_position.x,
+                    y: head_position.y + 1,
+                };
+            }
+            "D" => {
+                head_position = Position {
+                    x: head_position.x,
+                    y: head_position.y - 1,
+                };
+            }
+            _ => bail!("Invalid direction! This should never happen..."),
+        };
+
+        updated_knots.insert(0, head_position);
+
+        for &tail_position in &knots[1..] {
+            // Conditionally adjust the tail.
+            let mut new_position = tail_position;
+            let offset = head_position - new_position;
+            let mut abs_offset = offset;
+            abs_offset.abs();
+            new_position = match abs_offset {
+                Position { x: 0, y: 0 }
+                | Position { x: 1, y: 0 }
+                | Position { x: 0, y: 1 }
+                | Position { x: 1, y: 1 } => new_position,
+                Position { x, y: _ } if x > 1 => {
+                    // Horizontal motion.
+                    Position {
+                        x: head_position.x
+                            + match head_position.x {
+                                x if x > new_position.x => -1,
+                                _ => 1,
+                            },
+                        y: head_position.y,
+                    }
+                }
+                Position { x: _, y } if y > 1 => {
+                    // Vertical motion.
+                    Position {
+                        x: head_position.x,
+                        y: head_position.y
+                            + match head_position.y {
+                                y if y > new_position.y => -1,
+                                _ => 1,
+                            },
+                    }
+                }
+                _ => {
+                    bail!("Unexpected motion: abs_offset = {abs_offset:?}, offset = {offset:?}");
+                }
+            };
+
+            head_position = new_position;
+            updated_knots.insert(updated_knots.len(), new_position);
+        }
+
+        Ok(updated_knots)
+    }
+
+    /// Parses string input and simulates the rope's motions.
+    fn parse(s: &str, rope_length: usize) -> Result<Self, Error> {
+        if rope_length < 2 {
+            bail!(
+                "Rope has to have a length of at least 2! {} requested.",
+                rope_length
+            );
+        }
+
+        let knots = vec![Position { x: 0, y: 0 }; rope_length];
+
+        let Some(&tail_position) = knots.last() else {
+            bail!("No tail knot found!");
+        };
+
         let mut tail_positions_visited = vec![tail_position];
+        let mut updated_knots = knots;
 
         for line in s.lines() {
             let line = line.trim();
@@ -139,78 +283,19 @@ impl FromStr for Rope {
 
                     // Process the offset one by one, because the path the tail takes depends on
                     // each individual movement of the head, NOT its final position.
-                    for _i in 0..num_moves {
-                        // Move the head.
-                        match *direction {
-                            "R" => {
-                                head_position = Position {
-                                    x: head_position.x + 1,
-                                    y: head_position.y,
-                                };
-                            }
-                            "L" => {
-                                head_position = Position {
-                                    x: head_position.x - 1,
-                                    y: head_position.y,
-                                };
-                            }
-                            "U" => {
-                                head_position = Position {
-                                    x: head_position.x,
-                                    y: head_position.y + 1,
-                                };
-                            }
-                            "D" => {
-                                head_position = Position {
-                                    x: head_position.x,
-                                    y: head_position.y - 1,
-                                };
-                            }
-                            _ => bail!("Invalid direction! This should never happen..."),
+                    for i in 0..num_moves {
+                        let Some(&prev_tail) = updated_knots.last() else {
+                            bail!("Unable to grab current tail at beginning of move #{}", i);
                         };
 
-                        // Conditionally adjust the tail.
-                        let offset = head_position - tail_position;
-                        let mut abs_offset = offset;
-                        abs_offset.abs();
-                        let mut tail_adjusted = false;
-                        tail_position = match abs_offset {
-                            Position { x: 0, y: 0 }
-                            | Position { x: 1, y: 0 }
-                            | Position { x: 0, y: 1 }
-                            | Position { x: 1, y: 1 } => tail_position,
-                            Position { x, y: _ } if x > 1 => {
-                                // Horizontal motion.
-                                tail_adjusted = true;
-                                Position {
-                                    x: head_position.x
-                                        + match head_position.x {
-                                            x if x > tail_position.x => -1,
-                                            _ => 1,
-                                        },
-                                    y: head_position.y,
-                                }
-                            }
-                            Position { x: _, y } if y > 1 => {
-                                // Vertical motion.
-                                tail_adjusted = true;
-                                Position {
-                                    x: head_position.x,
-                                    y: head_position.y
-                                        + match head_position.y {
-                                            y if y > tail_position.y => -1,
-                                            _ => 1,
-                                        },
-                                }
-                            }
-                            _ => {
-                                bail!("Unexpected motion: abs_offset = {abs_offset:?}, offset = {offset:?}");
-                            }
+                        updated_knots = Self::get_updated_positions(direction, &updated_knots)?;
+
+                        let Some(&new_tail) = updated_knots.last() else {
+                            bail!("Unable to grab current tail at end of move #{}", i);
                         };
 
-                        // Record the position if it was adjusted.
-                        if tail_adjusted {
-                            tail_positions_visited.push(tail_position);
+                        if new_tail != prev_tail {
+                            tail_positions_visited.push(new_tail);
                         }
                     }
                 }
@@ -222,9 +307,7 @@ impl FromStr for Rope {
             tail_positions_visited,
         })
     }
-}
 
-impl Rope {
     /// Gets the number of positions the tail of the rope visited at least once.
     pub fn get_number_of_positions_rope_tail_visits_at_least_once(&self) -> usize {
         self.tail_positions_visited
@@ -253,13 +336,13 @@ impl Display for Rope {
                     origin.y = o.y.max(p.y.abs());
                 }
 
-                let width = w.max(
-                    (1 + p.x.abs() + origin.x)
+                let width = 1 + w.max(
+                    (p.x.abs() + origin.x)
                         .try_into()
                         .map_err(|_| std::fmt::Error)?,
                 );
-                let height = h.max(
-                    (1 + p.y.abs() + origin.y)
+                let height = 1 + h.max(
+                    (p.y.abs() + origin.y)
                         .try_into()
                         .map_err(|_| std::fmt::Error)?,
                 );
