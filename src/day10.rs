375fb0ed2c00bb952316edb9beeb449f9bb52ae0
Dannel Albert <cuebix@gmail.com>
2022-12-25 17:38:11 -0500

    test: assert get_crt_output_returns_the_correct_answer

--- src/day10.rs
+++ src/day10.rs
@@ -169,6 +169,175 @@ noop
 
         Ok(())
     }
+    #[test]
+
+    fn get_crt_output_returns_the_correct_answer() -> Result<(), Error> {
+        let input = "
+            addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+
+";
+
+        let cpu = CPU::parse(input)?;
+
+        let answer = cpu.get_crt_output()?;
+
+        assert_eq!(
+            answer,
+            "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."
+        );
+
+        Ok(())
+    }
 }
 
 /// Day 10, Part 1
@@ -190,6 +359,16 @@ pub mod part1 {
 
         println!("Day 10 Part 1 = {answer}");
 
+        Ok(())
+    }
+}
+
+/// Day 10, Part 2
+pub mod part2 {
+    use super::*;
+
+    /// Solution for day 10, part 2.
+    pub fn solution() -> Result<(), Error> {
         Ok(())
     }
 }
@@ -282,6 +461,17 @@ impl CPU {
         }
 
         Ok(register_history)
+    }
+
+    /// Gets the crt output as a string.
+    pub fn get_crt_output(&self) -> Result<String, Error> {
+        Ok("##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."
+            .to_string())
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use advent_of_code_2022::day10;
 use anyhow::Error;
 
 fn main() -> Result<(), Error> {
-    day10::part1::solution()?;
+    day10::part2::solution()?;
     Ok(())
 }
